bd54dfa52de238ca642e3acd32bd54edc5b69f9a
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-04-24 07:36:38 +0200

    Creando la vista listar y el fichero import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- Se tiene que llamar import.sql obligatoriamente para que se importe para H2
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Adri', 'Pérez', 'adri@hotmail.com', '2021-01-12');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'José', 'Muñoz', 'jmunoz@hotmail.com', '2022-10-30');
