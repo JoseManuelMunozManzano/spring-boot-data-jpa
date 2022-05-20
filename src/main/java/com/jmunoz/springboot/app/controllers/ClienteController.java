a56326a2e0f27f31caedf5d46a5305199942d991
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-20 20:47:02 +0200

    Aliminar archivo de imagen

--- src/main/java/com/jmunoz/springboot/app/controllers/ClienteController.java
+++ src/main/java/com/jmunoz/springboot/app/controllers/ClienteController.java
@@ -6,10 +6,13 @@ import com.jmunoz.springboot.app.util.paginator.PageRender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,7 +22,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -35,8 +40,10 @@ public class ClienteController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    private final static String UPLOADS_FOLDER = "uploads";
+
     // Para ver la foto
-    @GetMapping(value="/ver/{id}")
+    @GetMapping(value = "/ver/{id}")
     public String ver(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
 
         Cliente cliente = clienteService.findOne(id);
@@ -75,7 +82,41 @@ public class ClienteController {
         return "form";
     }
 
-    // MultipartFile es el tipo que hay que indicar para pasar files
+    // Otra forma de cargar imágenes de forma programática, a través de código Java usando el recurso
+    // InputStreamResource. Es un stream de entrada que contiene toda la información de la imagen en bytes.
+    // No usa la clase de configuración MvcConfig, por lo que se borra.
+    // Se usa un método handler en el controlador que se encarga de recibir la imagen como parámetro,
+    // la convierte a un recurso y la carga en la respuesta HTTP como un ResponseEntity.
+    //
+    // En la ruta irá el nombre del archivo.
+    // Esto se corresponde a lo que se ve en ver.html, th:src="@{'/uploads/' + ${cliente.foto}}"
+    // La expresión regular :.+ evita que Spring borre la extensión del archivo
+    @GetMapping(value = "/uploads/{filename:.+}")
+    public ResponseEntity<Resource> verFoto(@PathVariable String filename) {
+        // Convertimos la imagen a un path absoluto para poder cargarla
+        // resolve ya incluye el /
+        Path pathFoto = Paths.get(UPLOADS_FOLDER).resolve(filename).toAbsolutePath();
+        log.info("pathFoto: " + pathFoto);
+
+        Resource recurso = null;
+        try {
+            // Se carga la imagen
+            recurso = new UrlResource(pathFoto.toUri());
+            if (!recurso.exists() || !recurso.isReadable()) {
+                throw new RuntimeException("Error: no se puede cargar la imagen: " + pathFoto.toString());
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // Devolvemos el archivo en la respuesta HTTP. Se anexa al body
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + recurso.getFilename() + "\"")
+                .body(recurso);
+    }
+
+    // Esta parte sigue quedando como esta porque es la parte de guardar en la ruta.
+    // La parte nueva es la de cargar.
     @RequestMapping(value = "/form", method = RequestMethod.POST)
     public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
                           @RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
@@ -85,34 +126,35 @@ public class ClienteController {
             return "form";
         }
 
-        // Se cambia directorio uploads externo al proyecto por un directorio absoluto y externo en la raiz
-        // del proyecto.
-        // Se ha creado el directorio uploads en la raiz del proyecto.
-        // Notar que cuando se haga el deploy no contiene imágenes. Estan dentro del proyecto de desarrollo,
-        // no de producción
-        // Para ver la configuración de esa carpeta ver la clase MvcConfig
         if (!foto.isEmpty()) {
-            // Generamos un nombre nuevo de fichero para evitar que se sobresescriba un fichero si ya existe
+            // Se elimina la antigua foto y se guarda la nueva
+            if (cliente.getId() != null
+                    && cliente.getId() > 0
+                    && cliente.getFoto() != null
+                    && cliente.getFoto().length() > 0) {
+                // Para borrar la imagen primero obtenemos su ruta absoluta, de él obtenemos el archivo
+                // y si existe y se puede leer se pregunta si se eliminó y se manda un mensaje flash
+                Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+                File archivo = rootPath.toFile();
+                if (archivo.exists() && archivo.canRead()) {
+                    archivo.delete();
+                }
+            }
+
             String uniqueFilename = UUID.randomUUID().toString() + "_" + foto.getOriginalFilename();
 
-            // Con resolve se concatena de forma automática al uploads el nombre del archivo.
-            // Es un Path relativo al proyecto.
-            Path rootPath = Paths.get("uploads").resolve(uniqueFilename);
-            // Se concatena al path absoluto donde está el proyecto
+            Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(uniqueFilename);
             Path rootAbsolutePath = rootPath.toAbsolutePath();
 
-            // Para hacer debug de los nombres de directorio
             log.info("rootPath: " + rootPath);
             log.info("rootAbsolutePath: " + rootAbsolutePath);
 
             try {
-                // Otra forma de generar el archivo en la ruta, usando el stream de entrada y el path absoluto
                 Files.copy(foto.getInputStream(), rootAbsolutePath);
 
                 flash.addFlashAttribute("info", "Has subido correctamente '" +
                         uniqueFilename + "'");
 
-                // Pasamos el nombre de la foto al entity Cliente para que quede guardado en la BBDD
                 cliente.setFoto(uniqueFilename);
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -132,7 +174,7 @@ public class ClienteController {
     public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
         Cliente cliente = null;
 
-        if (id > 0 ){
+        if (id > 0) {
             cliente = clienteService.findOne(id);
             if (cliente == null) {
                 flash.addFlashAttribute("error", "El ID del cliente no existe en la BBDD!");
@@ -152,8 +194,20 @@ public class ClienteController {
     public String eliminar(@PathVariable Long id, RedirectAttributes flash) {
 
         if (id > 0) {
+            Cliente cliente = clienteService.findOne(id);
+
             clienteService.delete(id);
             flash.addFlashAttribute("success", "Cliente eliminado con éxito!");
+
+            // Para borrar la imagen primero obtenemos su ruta absoluta, de él obtenemos el archivo
+            // y si existe y se puede leer se pregunta si se eliminó y se manda un mensaje flash
+            Path rootPath = Paths.get(UPLOADS_FOLDER).resolve(cliente.getFoto()).toAbsolutePath();
+            File archivo = rootPath.toFile();
+            if (archivo.exists() && archivo.canRead()) {
+                if (archivo.delete()) {
+                    flash.addFlashAttribute("info", "Foto " + cliente.getFoto() + " eliminada con éxito!");
+                }
+            }
         }
 
         return "redirect:/listar";
