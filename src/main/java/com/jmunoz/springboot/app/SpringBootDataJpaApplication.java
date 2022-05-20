8f95650b2906bd85e2e6e2e1348ed9a2c4d4a294
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-20 21:23:46 +0200

    Inicializando directorio uploads automáticamente

--- src/main/java/com/jmunoz/springboot/app/SpringBootDataJpaApplication.java
+++ src/main/java/com/jmunoz/springboot/app/SpringBootDataJpaApplication.java
@@ -1,13 +1,33 @@
 package com.jmunoz.springboot.app;
 
+import com.jmunoz.springboot.app.models.service.IUploadFileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Se implementa la interfaz CommandLineRunner para que se cree automáticamente el
+// directorio uploads, contenedor de imágenes
+// Se ha borrado físicamente el directorio uploads, ya que ahora la app lo crea.
+// Esto para testing está muy bien, ya que cada vez que se cierre la app desaparece el directorio y
+// cuando se inicie (o se lancen las pruebas) se genera.
+//
+// Ver Interface IUploadFileService, donde se añaden un par de métodos
+
 @SpringBootApplication
-public class SpringBootDataJpaApplication {
+public class SpringBootDataJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	IUploadFileService uploadFileService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
 	}
 
+	// Ejecuta lo primero de to-do. Sirve para hacer configuraciones, en este caso del directorio de imágenes
+	@Override
+	public void run(String... args) throws Exception {
+		uploadFileService.deleteAll();
+		uploadFileService.init();
+	}
 }
--- src/main/java/com/jmunoz/springboot/app/models/service/IUploadFileService.java
+++ src/main/java/com/jmunoz/springboot/app/models/service/IUploadFileService.java
@@ -6,14 +6,17 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-// Servicio para manejar el tema de subida/borrado de ficheros
 public interface IUploadFileService {
 
-    // El recurso se guarda en el ResponseEntity, en el método handle del controlador
     Resource load(String filename) throws MalformedURLException;
 
-    // Nombre único de la imagen
     String copy(MultipartFile file) throws IOException;
 
     boolean delete(String filename);
+
+    // Borra to-do el directorio uploads de forma recursiva
+    void deleteAll();
+
+    // Crea el directorio uploads
+    void init() throws IOException;
 }
