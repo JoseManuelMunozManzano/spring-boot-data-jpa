34d92597a0f4bcfe42d156136d0866fc1f6c130e
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-25 19:34:21 +0200

    Implementando método handler guardar en FacturaController

--- src/main/java/com/jmunoz/springboot/app/controllers/FacturaController.java
+++ src/main/java/com/jmunoz/springboot/app/controllers/FacturaController.java
@@ -2,11 +2,15 @@ package com.jmunoz.springboot.app.controllers;
 
 import com.jmunoz.springboot.app.models.entity.Cliente;
 import com.jmunoz.springboot.app.models.entity.Factura;
+import com.jmunoz.springboot.app.models.entity.ItemFactura;
 import com.jmunoz.springboot.app.models.entity.Producto;
 import com.jmunoz.springboot.app.models.service.IClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -19,6 +23,8 @@ public class FacturaController {
 
     @Autowired
     private IClienteService clienteService;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/form/{clienteId}")
     public String crear(@PathVariable(value = "clienteId") Long clienteId,
@@ -40,14 +46,45 @@ public class FacturaController {
         return "factura/form";
     }
 
-    // Autocomplete
-    // En resources/templates/factura/js/autocomplete-productos.html se indica
-    // url: "/factura/cargar-productos/" + request.term,
-    //
-    // @ResponseBody suprime la carga de una vista Thymeleaf y en vez de eso va a tomar el resultado
-    // convertido a JSON y lo va a registrar dentro del body de la respuesta
     @GetMapping(value = "/cargar-productos/{term}", produces = {"application/json"})
     public @ResponseBody List<Producto> cargarProductos(@PathVariable String term) {
         return clienteService.findByNombre(term);
+    }
+
+    // Guardar facturas con sus líneas en BD
+    // Los nombres de los parámetros que se reciben vienen de plantilla-items.html, pero notar que estos datos
+    // de plantilla se eliminan en autocomplete-productos.html para que no coja esa línea e intente grabarla,
+    // aunque daría error porque el value de item_id[] debe ser un número y en ese caso es ID.
+    //
+    // Factura es el objeto del formulario que se inyecta de forma automática al método. Contiene todos los
+    // datos del formulario excepto las líneas que se manejan aparte a través de un input, ya que no están
+    // directamente mapeadas a campos de la clase Factura.
+    // Por tanto, pasamos los items de la factura a la factura y la guardamos.
+    @PostMapping("/form")
+    public String guardar(Factura factura,
+                          @RequestParam(name = "item_id[]", required = false) Long[] itemId,
+                          @RequestParam(name = "cantidad[]", required = false) Integer[] cantidad,
+                          RedirectAttributes flash,
+                          SessionStatus status) {
+        for (int i=0; i<itemId.length; i++) {
+            Producto producto = clienteService.findProductoById(itemId[i]);
+
+            ItemFactura linea = new ItemFactura();
+            linea.setCantidad(cantidad[i]);
+            linea.setProducto(producto);
+
+            factura.addItemFactura(linea);
+
+            log.info("ID: " + itemId[i].toString() + ", cantidad: " + cantidad[i].toString());
+        }
+
+        clienteService.saveFactura(factura);
+
+        // Eliminamos la factura de la sesión
+        status.setComplete();
+
+        flash.addFlashAttribute("success", "Factura creada con éxito");
+
+        return "redirect:/ver/" + factura.getCliente().getId();
     }
 }
