221e9198cbd46fa45365b5fc4781b34d06ea59cd
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-29 17:36:27 +0200

    Security - Validando LoginController con los mensajes de error.

--- src/main/java/com/jmunoz/springboot/app/controllers/LoginController.java
+++ src/main/java/com/jmunoz/springboot/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.jmunoz.springboot.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,24 +12,21 @@ import java.security.Principal;
 @Controller
 public class LoginController {
 
-    // Esta es la página de login personalizada.
-    // Si to-do sale bien el usuario queda logeado.
-    // Si algo sale mal, tipo el usuario no existe o clave incorrecta, el mensaje de error también se
-    // puede personalizar en esta misma acción, ya que por defecto va a redirigir hacia este controlador
-    // con la url login, el texto del error y el problema que ocurrió.
-    // Esto se va a personalizar después.
-    //
-    // El objeto principal es muy importante. Contiene el usuario logueado y permite validar.
-    // Si el usuario ya ha iniciado sesión redirigimos a la página de inicio y no volvemos a mostrar
-    // la página de login, evitando un doble inicio de sesión.
-    //
-    // No olvidar habilitar la página en la configuración SpringSecurityConfig con la ruta indicada en el @GetMapping
+    // Se añade la gestión del error. El error viene en el request.
+    // No es requerido porque puede que no venga ningún error.
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash) {
+    public String login(@RequestParam(value = "error", required = false) String error,
+            Model model, Principal principal, RedirectAttributes flash) {
 
         if (principal != null) {
             flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriormente");
             return "redirect:/";
+        }
+
+        if (error != null) {
+            model.addAttribute("error",
+                    "Error en el login: Nombre de usuario o contraseña incorrecta. " +
+                            "Por favor, vuelva a intentarlo!");
         }
 
         return "login";
