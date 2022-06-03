b55ee7b1c74a1dc12a9ee0ad02f581f7fb3385a3
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-03 17:39:10 +0200

    XML - Configuración y creación de clase XML Wrapper

--- src/main/java/com/jmunoz/springboot/app/MvcConfig.java
+++ src/main/java/com/jmunoz/springboot/app/MvcConfig.java
@@ -2,6 +2,7 @@ package com.jmunoz.springboot.app;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,7 +10,6 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -25,29 +25,9 @@ public class MvcConfig implements WebMvcConfigurer {
         return new BCryptPasswordEncoder();
     }
 
-    // Configuración para implementar el multilenguaje (locale)
-    // 1. Se implementa el bean LocaleResolver que es una interface que se encarga del adaptador en el cual vamos a
-    //      guardar nuestro locale con nuestra internacionalización.
-    //      Por ejemplo, puede ser en una sesión http, en una cookie...
-    //      Es decir, donde se va a guardar el parámetro de nuestro lenguaje (locale).
-    // 2. También se implementa el bean LocaleChangeInterceptor que se encarga de modificar el locale cada vez
-    //      que se envía el parámetro del lenguaje con el nuevo idioma.
-    //      Un interceptor se ejecuta en cada request y se encarga de realizar alguna tarea justo antes de invocar un
-    //      método handler del controlador (como un middleware de NodeJs)
-    // 3. Finalmente, tenemos que implementar un método para registrar este interceptor.
-
     @Bean
     public LocaleResolver localeResolver() {
-        // En este caso se guarda en una sesión http (sessionLocalResolver)
-        // SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-
-        // Ejemplo guardando en Cookie el Locale
-        // Con esto se evita que, si estoy en el idioma Inglés, cuando pulso LogOut, como se pierde la sesión
-        // el idioma vuelva al por defecto (español)
-        // Aunque se pierda la sesión, como ya tengo el idioma en una Cookie, no se pierde el idioma.
         CookieLocaleResolver localeResolver = new CookieLocaleResolver();
-
-        // Se indica código del locale y el país
         localeResolver.setDefaultLocale(new Locale("es", "ES"));
         return localeResolver;
     }
@@ -55,8 +35,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
-        // Cada vez que se pase por url a través del método GET el parámetro lang, por ejemplo con el valor "es_ES"
-        // se va a ejecutar este interceptor y va a realizar el cambio de lenguaje.
         localeInterceptor.setParamName("lang");
         return localeInterceptor;
     }
@@ -64,5 +42,23 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    // Conversor que convierte un objeto en un XML (Marshalling)
+    // Se va a utilizar en nuestra vista XML para poder convertir nuestra respuesta (el objeto Entity) en un
+    // documento XML.
+    // XML no puede convertir un objeto Collection (como un List) en XML. Necesitamos una clase wrapper que envuelva
+    // nuestra lista.
+    // Si, por ejemplo, queremos convertir nuestro listado de clientes, necesitamos una clase ClienteList que envuelva
+    // el objeto List o Collection dentro de esta clase.
+    // Esta clase va a representar el XML root, la raiz del documento XML.
+    //
+    // Tanto la clave wrapper como la clave vista están en el package view/xml
+    @Bean
+    public Jaxb2Marshaller jaxb2Marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        // Las clases que se van a convertir en XML (la clase wrapper)
+        marshaller.setClassesToBeBound(new Class[] {com.jmunoz.springboot.app.view.xml.CilenteList.class});
+        return marshaller;
     }
 }
---
+++ src/main/java/com/jmunoz/springboot/app/view/xml/CilenteList.java
@@ -0,0 +1,31 @@
+package com.jmunoz.springboot.app.view.xml;
+
+import com.jmunoz.springboot.app.models.entity.Cliente;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+// Esta es nuestra clase wrapper
+// Si no se le asigna un nombre, por defecto toma el nombre de la clase
+@XmlRootElement(name = "clientes")
+public class CilenteList {
+
+    // Tenemos que indicar cuál es el atributo que va a ser un elemento XML por cada objeto cliente que tengamos
+    @XmlElement(name = "cliente")
+    public List<Cliente> clientes;
+
+    // Importante que tenga el constructor vacío para que lo pueda manejar el Jaxb2Marshaller, el Bean encargado
+    // de convertir
+    public CilenteList() {
+    }
+
+    public CilenteList(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    // Solo hace falta el método get
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+}
