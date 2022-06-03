72479def26ad52eddba39f0130ffdc8a51132bbb
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-03 12:20:12 +0200

    Excel - Escribiendo el código del detalle de la Factura en la vista Excel

--- src/main/java/com/jmunoz/springboot/app/view/xlsx/FacturaXlsxView.java
+++ src/main/java/com/jmunoz/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -1,6 +1,7 @@
 package com.jmunoz.springboot.app.view.xlsx;
 
 import com.jmunoz.springboot.app.models.entity.Factura;
+import com.jmunoz.springboot.app.models.entity.ItemFactura;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -48,5 +49,25 @@ public class FacturaXlsxView extends AbstractXlsxView {
         sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
         sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
         sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
+
+        // Detalle de la factura
+        Row header = sheet.createRow(9);
+        header.createCell(0).setCellValue("Producto");
+        header.createCell(1).setCellValue("Precio");
+        header.createCell(2).setCellValue("Cantidad");
+        header.createCell(3).setCellValue("Total");
+
+        int rownum = 10;
+        for (ItemFactura item: factura.getItems()) {
+            Row fila = sheet.createRow(rownum++);
+            fila.createCell(0).setCellValue(item.getProducto().getNombre());
+            fila.createCell(1).setCellValue(item.getProducto().getPrecio());
+            fila.createCell(2).setCellValue(item.getCantidad());
+            fila.createCell(3).setCellValue(item.calcularImporte());
+        }
+
+        Row filaTotal = sheet.createRow(rownum);
+        filaTotal.createCell(2).setCellValue("Gran Total: ");
+        filaTotal.createCell(3).setCellValue(factura.getTotal());
     }
 }
