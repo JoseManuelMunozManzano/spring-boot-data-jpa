ae11652a3a6b51440294290469d8502adb91b512
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-06-04 07:05:00 +0200

    API REST - RestController

--- src/main/java/com/jmunoz/springboot/app/SpringSecurityConfig.java
+++ src/main/java/com/jmunoz/springboot/app/SpringSecurityConfig.java
@@ -33,8 +33,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Con listar** también se da autorización a nuestra nueva ruta listar-rest
-        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale").permitAll()
+        // Agregamos la seguridad para nuestro RestController
+        http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale", "/api/clientes/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
---
+++ src/main/java/com/jmunoz/springboot/app/controllers/ClienteRestController.java
@@ -0,0 +1,30 @@
+package com.jmunoz.springboot.app.controllers;
+
+import com.jmunoz.springboot.app.models.service.IClienteService;
+import com.jmunoz.springboot.app.view.xml.CilenteList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controlador específico dedicado a un API Rest
+// En vez de usar la anotación @Controller se usa la anotación @RestController, que combina 2 anotaciones,
+// @Controller y @ResponseBody. Por lo tanto, to-dos sus métodos van a ser @ResponseBody
+// Para probar
+//  XML:   http://localhost:8080/api/clientes/listar
+//  JSON:  http://localhost:8080/api/clientes/listar?format=json
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteRestController {
+
+    @Autowired
+    private IClienteService clienteService;
+
+    // Ya no hace falta anotar con @ResponseBody porque ya la maneja @RestController
+    @GetMapping(value = "/listar")
+    public CilenteList listar() {
+
+        return new CilenteList(clienteService.findAll());
+    }
+}
